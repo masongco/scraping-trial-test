4730a037aeb9598f2701528c7dd547630964fe64
masongco <markallensongco@gmail.com>
2026-01-24 21:45:38 +0800

    implement basic page fetching using requests

--- scraper.py
+++ scraper.py
@@ -1,5 +1,13 @@
+import requests
+
+def fetch_page(url):
+    response = requests.get(url)
+    return response.txt
+
 def main():
-    pass
+    url = "https://scraping-trial-test.vercel.app"
+    html = fetch_page(url)
+    print(len(html))
 
 
 if __name__ == "__main__":
